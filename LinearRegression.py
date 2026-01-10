2d7d0c383c704f395b97ba6dde7554e1d808c742
daniyal <daniyal_faheem@live.com>
2026-01-10 18:50:46 +0500

    read files

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,7 +1,19 @@
 import kagglehub
+import os
+import pandas as pd
 
 # Download latest version
 path = kagglehub.dataset_download(
     "dhrubangtalukdar/fortune-500-companies-stock-data")
 
-print("Path to dataset files:", path)
+file_path = os.path.join(path, "fortune_500_stock_data.csv")
+df = pd.read_csv(file_path)
+
+
+print(df.head())
+print(df.info())
+# print("Path to dataset files:", path)
+# print(os.listdir(path))
+
+# inner_dir = os.path.join(path, "fortune_500_stock_data")
+# print("Inner files:", os.listdir(inner_dir))
